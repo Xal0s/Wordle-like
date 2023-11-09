d3381fc586d68a9c71116d93fe3aad0a8b69c3fc
Xal0s <kevinpereira95@free.fr>
2023-11-09 17:03:31 +0100

    menu principal assemblé avec le jeu + ecran victoire + score gardé en mémoire

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const launchGame = document.getElementsByClassName("new_game")
 const gameModes = document.getElementsByClassName("game_modes")
 const toggle = document.querySelector(".toggle")
 const chrono = document.querySelector(".etat_toggle")
-let timerActive = localStorage.getItem('timerActive') === 'true';
 function displayRules(){
     document.getElementById("modal_rules").style.left = "530px";
 }
@@ -19,35 +18,16 @@ function hideGameModes(){
     document.getElementById("menu_game_mode").style.left = "-500vh";
 }
 
-function activeTimer(){
-    console.log("active")
-    localStorage.setItem('timerActive', 'true');
-            
-    window.location.href = 'jeu.html';
-}
 
 function sliderToggle(){
     toggle.classList.toggle("active");
     
     if(toggle.classList.contains("active")){
         chrono.innerHTML = "Chrono ON";
-        activeTimer();
+        document.getElementById("toggle_button").style.left = "65px";
     }
     else{
         chrono.innerHTML = "Chrono OFF"
+        document.getElementById("toggle_button").style.left = "25px";
     }
-}
-
-
-if (timerActive) {
-    // Commencer le timer si activé
-    var startTime = new Date().getTime();
-
-    setInterval(function () {
-        var currentTime = new Date().getTime();
-        var elapsedTime = currentTime - startTime;
-
-        // Mettre à jour l'affichage du timer
-        document.getElementById('timer').innerText = 'Temps écoulé : ' + (elapsedTime / 1000) + ' secondes';
-    }, 100000);
 }
